f19cdd1dd9b5cc7ef8504ec228189267e86f73df
Jiuzhou  Tang <hgu145@jrl09.jureca>
2016-07-06 11:26:56 +0200

    test fftpack dct and idct, comparing crank nicolson with pesudo spetrum

--- polymer.py
+++ polymer.py
@@ -3,22 +3,29 @@
 MultiBlock
 =========
 
-:copyright: (c) 2015 by Jiuzhou Tang
+:copyright: (c) 2016 by Jiuzhou Tang
 :license: BSD, see LICENSE for more details.
 
 """
 
 import numpy as np
 from scipy import optimize
+from scipy import fftpack
 import scipy.io
 import matplotlib.pyplot as pl
+from numpy.linalg import inv
+import scipy as sc
+import scipy.sparse as sparse
+import scipy.sparse.linalg
+
 import time
 
 def polymer_scft(D):
-    Nx=100
+    Nx=200
     Ns=200
     qf_y=np.zeros((Ns+1,Nx,Nx))
     qf=np.zeros((Ns+1,Nx))
+    qfr=np.zeros((Ns+1,Nx+1))
     qb=np.zeros((Ns+1,Nx))
     rhof=np.zeros((Ns+1,Nx,Nx))
     rho=np.zeros((Nx,Nx))
@@ -34,28 +41,38 @@ def polymer_scft(D):
     kx=np.arange(Nx,dtype=float)
     kx[:half_kx+1]=kx[:half_kx+1]*d_kx
     kx[half_kx+1:]=d_kx*(Nx-kx[half_kx+1:])
-    for i in np.arange(Nx):
-    #for i in np.arange(Nx/2):
-        chemw[i]=3.0*np.pi**2*(i*dx)**2
+    #for i in np.arange(Nx):
+        #chemw[i]=3.0*np.pi**2*(i*dx)**2
         #chemw[Nx-i-1]=chemw[i]
     for i in np.arange(Nx):
         exp_w[i]=np.exp(chemw[i]*(-0.5*ds))
     #pl.plot(exp_w[:],'r')
-    pl.plot(-1.0*chemw[:],'r')
-    pl.show()
     # initial condition for propagator qf[z,z0,s] in ref[J.C.P vol 117,No. 5]
     local_data=np.zeros((Nx),dtype=complex)
+    local_data_rin=np.zeros((Nx))
+    local_data_rout=np.zeros((Nx))
     for x in np.arange(Nx) :
         k2=kx[x]**2
         exp_k2[x]=np.exp(-1.0*ds*k2)
 
     qf[0,:]=1.0
+    for x in np.arange(Nx):
+        qf[0,x]=np.exp(-2000.0*((x-60)*1.0/Nx)**2)*Nx
+        chemw[x]=-30*np.cos(np.pi*x*2.0/Nx)
+        exp_w[x]=np.exp(chemw[x]*(-0.5*ds))
+    pl.plot(exp_w[:],'r')
+    pl.show()
     for s in np.arange(1,Ns+1):
-        local_data[:]=qf[s-1,:]*exp_w[:]+0.0j
-        local_data=np.fft.fftn(local_data)
-        local_data[:]=local_data[:]*exp_k2[:]
-        local_data=np.fft.ifftn(local_data)
-        qf[s,:]=local_data[:].real*exp_w[:]
+        #local_data[:]=qf[s-1,:]*exp_w[:]+0.0j
+        local_data_rin[:]=qf[s-1,:]*exp_w[:]
+        #local_data=np.fft.fftn(local_data)
+        local_data_rin=fftpack.dct(x, type=2, norm='ortho')
+        #local_data[:]=local_data[:]*exp_k2[:]
+        local_data_rin[:]=local_data_rin[:]*exp_k2[:]
+        #local_data=np.fft.ifftn(local_data)
+        local_data_rout=fftpack.idct(local_data_rin, type=2, norm='ortho')
+        qf[s,:]=local_data_rout[:]*exp_w[:]
+        #qf[s,:]=local_data[:].real*exp_w[:]
     qb[Ns,:]=0.0
     
     for z0 in np.arange(Nx) :
@@ -100,7 +117,7 @@ def polymer_scft(D):
     #        for s in np.arange(Ns+1):
     #            rhof[s,z0,z]=qf_y[s,z0,z]*qf_y[Ns-s,z,0]/qf_y[Ns,z0,0]
     #pl.plot(qf_y[0,50,:])
-    pl.plot(qf_y[30,90,:])
+    #pl.plot(qf_y[30,90,:])
     #pl.plot(qf_y[1,50,:])
     #pl.plot(qf_y[3,50,:])
     #pl.plot(qf_y[5,50,:])
@@ -115,7 +132,28 @@ def polymer_scft(D):
     #pl.plot(rhof[30,50,:])
     #pl.plot(rhof[35,50,:])
     #pl.plot(rhof[40,50,:])
+    #pl.show()
+    qfr[0,:]=1.0 
+    #qfr[0,90]=1.0*Nx 
+    chemw[:]=1.0
+    for x in np.arange(Nx):
+        qfr[0,x]=np.exp(-2000.0*((x-60)*1.0/Nx)**2)*Nx
+        chemw[x]=-30*np.cos(np.pi*x*2.0/Nx)
+    crank_nicolson(Nx,D,Ns,ds,chemw,qfr)
+    #pl.plot(qfr[:,50])
+    #pl.plot(qfr[1,:],'y')
+    #pl.plot(qfr[5,:])
+    #pl.plot(qfr[10,:])
+    pl.plot(qf[50,:],'b')
+    pl.plot(qfr[50,:],'r^')
+    #pl.plot(chemw,'b^')
+    #pl.plot(qfr[20,:])
+    #pl.plot(qfr[40,:])
+    #pl.plot(qf_y[30,90,:])
+    Ws=np.append(chemw[:],chemw[0]) 
+    #pl.plot(0.1*Ws[:],'bo')
     pl.show()
+    print "chemw[0],chemw[99]",chemw[0],chemw[1],chemw[98],chemw[99]
     return
 
 
@@ -138,7 +176,8 @@ def crank_nicolson(Nx,Lx,Nt,dt,chemw,q):
         # Second-Derivative Matrix
         data = np.ones((3, Nx+1))
         data[1] = -2*data[1]
-        data[1,:]=data[1,:]-chemw[:]*(dx**2)
+        Ws=np.append(chemw[:],chemw[0]) 
+        data[1,:]=data[1,:]-Ws[:]*(dx**2)
         # Reflective boundary 
         data[2,1]=2*data[2,1]
         data[0,Nx-1]=2*data[0,Nx-1]
@@ -149,7 +188,7 @@ def crank_nicolson(Nx,Lx,Nt,dt,chemw,q):
         b = ( I + dt/2*D2 )*u
         
         u = np.transpose(np.mat( sparse.linalg.spsolve( A,  b ) ))
-        q[i,:,0,0]=np.reshape(u[:,0],(Nx+1))
+        q[i,:]=np.reshape(u[:,0],(Nx+1))
 
 
 
